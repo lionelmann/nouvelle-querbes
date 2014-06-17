914768e8b4d6cb05526b13d77bc2ee545c43bcf1
ajay <ajay786singh@gmail.com>
2014-06-17 17:22:30 +0530

    style add for inner page

--- wordpress/wp-content/themes/base/js/build/search.js
+++ wordpress/wp-content/themes/base/js/build/search.js
@@ -1,17 +1,33 @@
 jQuery(document).ready(function($){
 	$('.search a').click(function(){
 		$(this).toggleClass('active');
-		var src = $('.logo a img').attr('src');
-		var logo_1=template_url+"/images/logo.png";
-		var logo_2=template_url+"/images/ecole-logo.png";
-		if(src==logo_1) {
-			$('.logo a img').attr('src',logo_2);
-			$('.logo a').addClass('active-logo');
-		}else {
-			$('.logo a img').attr('src',logo_1);
-			$('.logo a').removeClass('active-logo');
+		var body =$('body');
+		if(!body.hasClass('page')){
+			var src = $('.logo a img').attr('src');
+			var logo_1=template_url+"/images/logo.png";
+			var logo_2=template_url+"/images/ecole-logo.png";
+			if(src==logo_1) {
+				$('.logo a img').attr('src',logo_2);
+				$('.logo a').addClass('active-logo');
+			}else {
+				$('.logo a img').attr('src',logo_1);
+				$('.logo a').removeClass('active-logo');
+			}
 		}
-		$('#search').toggle();
+		
+		if(!body.hasClass('single')){
+			var src = $('.logo a img').attr('src');
+			var logo_1=template_url+"/images/logo.png";
+			var logo_2=template_url+"/images/ecole-logo.png";
+			if(src==logo_1) {
+				$('.logo a img').attr('src',logo_2);
+				$('.logo a').addClass('active-logo');
+			}else {
+				$('.logo a img').attr('src',logo_1);
+				$('.logo a').removeClass('active-logo');
+			}
+		}
+		$('#search-box').slideToggle();
 		return false;
 	});
 });
--- wordpress/wp-content/themes/base/page.php
+++ wordpress/wp-content/themes/base/page.php
@@ -1,77 +1,17 @@
-<?php get_header(); ?>
+<?php get_header('inner'); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <?php // Get the feature banner image
-        if (has_post_thumbnail()) {
-            $image_id = get_post_thumbnail_id();
-            $image_url = wp_get_attachment_image_src($image_id,'banner', true);
-    ?>
-    <div class="banner">
-        <span class="bannerimage" style="background-image:url('<?php echo $image_url[0]; ?>'); display: block;">
-            <span class="inner"></span>
-        </span>
-    </div>
-    <?php } ?>
     <div class="container">
-        <section class="span-10 center">
-            <?php the_title( '<h4 class="headline">', '</h4>' ); ?>
-        </section>
-		<section class="span-10 center overflow-visible">
-				<div class="span-7 content">
+		<section class="span-10 center overflow-visible content">
+				<div class="span-7">
+					<?php the_title( '<h4 class="headline">', '</h4>' ); ?>
+					<div class="white-bg">
 					<?php the_content();?>
+					</div>
 				</div>
-				<div class="span-5 overflow-visible">
-					<div class="box light-green-2">
-						<h6>Avis Important</h6>
-						<?php query_posts('cat=3&showposts=1');
-								if(have_posts()):
-						?>
-							<ul class="posts">	
-								<?php while(have_posts()):the_post(); ?>
-									<li>
-										<?php the_title('<p class="post-title">','</p>');?>
-										<?php echo get_excerpt(200); ?>
-									</li>
-								<?php endwhile; ?>
-							</ul>
-						<a href="<?php echo get_category_link(3);?>" class="read-more">Read More</a>
-						<?php endif; wp_reset_query(); ?>
-					</div>	
-					<div class="box green">
-						<h6>ÉVÉNEMENTS À VENIR</h6>
-						<?php query_posts('post_type=tribe_events&showposts=5');
-							if(have_posts()):
-						?>
-							<ul class="events">
-								<?php while(have_posts()):the_post(); ?>
-									<li>
-										<p class="meta"><?php echo get_the_date('d.m.Y');?></p>
-										<p><a href="<?php the_permalink();?>"><?php the_title();?> >></a></p>
-									</li>
-								<?php endwhile; ?>
-							</ul>
-						<a href="" class="read-more">Read More</a>
-						<?php endif; wp_reset_query(); ?>
-					</div>
-					
-					<div class="box light-green">
-						<h6>COURRIER du jeudi</h6>
-						<?php query_posts('cat=2&showposts=3');
-								if(have_posts()):
-						?>
-							<ul class="posts">	
-								<?php while(have_posts()):the_post(); ?>
-									<li>
-										<?php the_title('<p class="post-title">','</p>');?>
-										<?php echo get_excerpt(100); ?>
-									</li>
-								<?php endwhile; ?>
-							</ul>
-						<a href="<?php echo get_category_link(2);?>" class="read-more">Read More</a>
-						<?php endif; wp_reset_query(); ?>
-					</div>
-			
+				<div class="span-5 overflow-visible sidebar">
+					<?php get_sidebar();?>
 				</div>
 		</section>
     </div>
