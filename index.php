80fa5377934a9fb4496f8561f924662daae7bb53
Nicolas <nicolas.beauchemin@gmail.com>
2015-01-08 12:27:33 -0500

    check de la provenance

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+if(!isset($_SERVER['HTTP_REFERER']) or strpos($_SERVER['HTTP_REFERER'],'http://www.nouvellequerbes.org/')===false) header("Location:http://www.nouvelle-querbes.com");
+
+
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
@@ -15,5 +19,4 @@ define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wordpress/wp-blog-header.php' );
-//Touch
 
