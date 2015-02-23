6e509040b8e4ff7fc5aa4be94236faa0f5e209de
Nicolas <nicolas.beauchemin@gmail.com>
2015-02-23 09:26:41 -0500

    Correction main menu

--- wordpress/wp-content/themes/nouvelle/includes/functions/breadcrumbs.php
+++ wordpress/wp-content/themes/nouvelle/includes/functions/breadcrumbs.php
@@ -6,7 +6,7 @@ function the_breadcrumb() {
         echo '<li class="home-link"><a href="';
         echo get_option('home');
         echo '">';
-        echo 'Accuiel';
+        echo 'Accueil';
         echo '</a></li><li class="separator"> // </li>';
         if (is_category() || is_single()) {
             echo '<li>';
