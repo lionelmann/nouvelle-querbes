0f034fd13bb8ac0aa48f3dfe8921e52a452da557
Lionel Mann <lionelmann@gmail.com>
2015-05-05 11:10:33 -0400

    fixes

--- wordpress/wp-content/themes/nouvelle/header.php
+++ wordpress/wp-content/themes/nouvelle/header.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="description" content="Site web officiel de l'école alternative Nouvelle-Querbes. Vous y retrouverez toute l'information à propos de la vie à l'école et des inscriptions ainsi que les communications officielles." />
     <title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo('name') ?></title>
     <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/favicon.ico" />
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class('animated fadeIn'); ?>>
+<?php include_once("analyticstracking.php") ?>
 
 <section role="banner">
     <header>
--- wordpress/wp-content/themes/nouvelle/includes/functions/breadcrumbs.php
+++ wordpress/wp-content/themes/nouvelle/includes/functions/breadcrumbs.php
@@ -1,14 +1,14 @@
 <?php
 function the_breadcrumb() {
     global $post;
-    echo '<ul id="breadcrumbs">';
+    echo '<ul>';
     if (!is_home()) {
         echo '<li class="home-link">';
         echo '<a href="';
         echo get_option('home');
         echo '">';
         bloginfo('name');
-        echo "</a></li>  // ";
+        echo "</a></li><span class='separator'>//</span>";
         if (is_category() || is_single()) {
             the_category(' ');
             if (is_single()) {
@@ -20,10 +20,10 @@ function the_breadcrumb() {
                 $anc = get_post_ancestors( $post->ID );
                 $title = get_the_title();
                 foreach ( $anc as $ancestor ) {
-                    $output = '<a href="'.get_permalink($ancestor).'" title="'.get_the_title($ancestor).'">'.get_the_title($ancestor).'</a>';
+                    $output = '<li><a href="'.get_permalink($ancestor).'">'.get_the_title($ancestor).'</a></li>';
                 }
                 echo $output;
-                echo '<span title="'.$title.'"> '.$title.'</span>';
+                echo '<span class="separator">//</span><li> '.$title.'</li>';
             } else {
                 echo '<li><span> '.get_the_title().'</span></li>';
             }
@@ -31,47 +31,3 @@ function the_breadcrumb() {
     }
     echo '</ul>';
 }
-
-/*
-function the_breadcrumb() {
-    global $post;
-    echo '<ul id="breadcrumbs">';
-    if (!is_home()) {
-        echo '<li class="home-link"><a href="';
-        echo get_option('home');
-        echo '">';
-        echo 'Accueil';
-        echo '</a></li><li class="separator"> // </li>';
-        if (is_category() || is_single()) {
-            echo '<li>';
-            the_category(' </li><li class="separator"> > </li><li> ');
-            if (is_single()) {
-                echo '</li><li class="separator"> > </li><li>';
-                the_title();
-                echo '</li>';
-            }
-        } elseif (is_page()) {
-            if($post->post_parent){
-                $anc = get_post_ancestors( $post->ID );
-                $title = get_the_title();
-                foreach ( $anc as $ancestor ) {
-                    $output = '<li><a href="'.get_permalink($ancestor).'" title="'.get_the_title($ancestor).'">'.get_the_title($ancestor).'</a></li> <li class="separator">></li>';
-                }
-                echo $output;
-                echo '<span title="'.$title.'"> '.$title.'</span>';
-            } else {
-                echo '<li><span> '.get_the_title().'</span></li>';
-            }
-        }
-    }
-    elseif (is_tag()) {single_tag_title();}
-    elseif (is_day()) {echo"<li>Archive for "; the_time('F jS, Y'); echo'</li>';}
-    elseif (is_month()) {echo"<li>Archive for "; the_time('F, Y'); echo'</li>';}
-    elseif (is_year()) {echo"<li>Archive for "; the_time('Y'); echo'</li>';}
-    elseif (is_author()) {echo"<li>Author Archive"; echo'</li>';}
-    elseif (isset($_GET['paged']) && !empty($_GET['paged'])) {echo "<li>Blog Archives"; echo'</li>';}
-    elseif (is_search()) {echo"<li>Search Results"; echo'</li>';}
-    echo '</ul>';
-}
-*/
-?>
